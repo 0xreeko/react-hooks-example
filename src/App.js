b4012e33735b444254ba4e7a6ffc33794aadd4a4
enrict <enric_trillo@outlook.com>
2021-08-12 18:39:37 +0100

    first - hooks examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [binder, setBinder] = useState("");
+
+  useEffect(() => {
+    // Using the useEffect hook as a way to update the site title
+    document.title = `useEffect updated ${counter} times`;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>onClick and useState</h2>
+      <p>Counter: {counter}</p>
+      <div className="button-group">
+        {/* Using the useState hook to update counter value */}
+        <button onClick={() => setCounter(counter + 1)}>Increment</button>
+        <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      </div>
+
+      <div className="binding-group">
+        {/* using useState and onChange to bind input */}
+        <h2>onChange and useState</h2>
+        <p>{binder}</p>
+        <input type="text" onChange={(e) => setBinder(e.target.value)} />
+      </div>
     </div>
   );
 }
